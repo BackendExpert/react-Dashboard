78f1eda8795681add1491a7a2b33e5eab675ea55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 13:35:41 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/DashSide.jsx
+++ src/components/Dashboard/DashSide.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DashSideImag from '../../assets/DashSide.jpg'
 
 const DashSide = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <div className="">
-        <div className={`lg:min-h-screen lg:pr-16 md:relative fixed overflow-auto top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
-            Hi all
+        <div style={{ backgroundImage: '../../assets/DashSide.jpg'  }} className={`bg-[url(../../assets/DashSide.jpg)] lg:min-h-screen lg:pr-16 md:relative fixed overflow-auto top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
+           <div className="">
+
+           </div>
         </div>
     </div>
   )
