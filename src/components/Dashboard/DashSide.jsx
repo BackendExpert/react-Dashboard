a6a88cb984f77fcfec2e82183d77750be16ba0be
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 13:56:32 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/DashSide.jsx
+++ src/components/Dashboard/DashSide.jsx
@@ -25,7 +25,7 @@ const DashSide = () => {
         </button>
         <div style={divStyle} className={`w-full md:w-auto md:min-h-screen md:pr-16 md:relative fixed overflow-auto top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
            <div className="py-4">
-                jehan Weerasuriya
+                <h1 className="">J. Weerasuriya</h1>
            </div>
         </div>
     </div>
