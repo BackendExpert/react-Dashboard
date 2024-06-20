0aa5b1eded177633efc7b24b46c1d930fd4a0dda
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 14:02:16 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/DashSide.jsx
+++ src/components/Dashboard/DashSide.jsx
@@ -16,6 +16,19 @@ const DashSide = () => {
         backgroundPosition: 'center'
     };
 
+    const SideLink = [
+        {id: 1, name: "Dashboard", link: "Home", icons: <BsSpeedometer2 />},    
+        {id: 2, name: "Leaves", link: "Leaves", icons: <BsBoxArrowDownRight />},  
+        {id: 3, name: "Reservation", link: "Reservation", icons: <BsTicketDetailedFill />},  
+        {id: 4, name: "SRNs", link: "SRNs", icons: <BsFileTextFill />},  
+        {id: 5, name: "Work Requests", link: "WorkRequests", icons: <BsHouseGearFill />},  
+        {id: 7, name: "GatePass", link: "GatePass", icons: <BsPassFill />},  
+        {id: 8, name: "Increment", link: "Increment", icons: <BsCashStack />},  
+        {id: 9, name: "Vehicle", link: "Vehicle", icons: <BsCarFrontFill />},  
+        {id: 10, name: "SiteAdmin", link: "SiteAdmins", icons: <BsPersonFillLock />},  
+        {id: 11, name: "Employee", link: "Employee", icons: <BsPeopleFill />},          
+    ]
+
   return (
     <div className="">
         <button className="md:hidden fixed top-4 right-4 z-50 bg-gray-600 text-white p-2 rounded font-semibold" onClick={toggleSidebar}>
@@ -26,6 +39,18 @@ const DashSide = () => {
         <div style={divStyle} className={`w-full md:w-auto md:min-h-screen md:pr-16 md:relative fixed overflow-auto top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
            <div className="py-4">
                 <h1 className="">J. Weerasuriya</h1>
+           </div>
+
+           <div className="my-4">
+                {
+                    SideLink.map((side) => {
+                        return (
+                            <div className="my-2 py-2 w-full cursor-pointer duration-500 hover:bg-gray-800 px-4">
+                                <h1 className="">{side.name}</h1>
+                            </div>
+                        )
+                    })
+                }
            </div>
         </div>
     </div>
