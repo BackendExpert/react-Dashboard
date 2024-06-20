f6bc45bf5edf3a6400c7c0b2a8d9a0dfa8bfa1d7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 15:54:21 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,20 +3,14 @@ import DashSide from './DashSide'
 import DashFooter from './DashFooter'
 import DashNav from './DashNav'
 import { Outlet } from 'react-router'
-import DashSideImag from '../../assets/DashSide.jpg'
 
 const Dashboard = () => {
-    const divStyle = {
-        backgroundImage: `linear-gradient(to right, #18181b, rgba(0,0,0,0.2)), url(${DashSideImag})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
-    };
+
   return (
     <div className="flex">
       <DashSide />
       {/* <DashSideTest /> */}
-      <div className="flex-1 bg-[#323132] " style={divStyle}>
+      <div className="flex-1 bg-gray-800">
         <DashNav />
         <div className="py-4 px-4">
             <Outlet />
