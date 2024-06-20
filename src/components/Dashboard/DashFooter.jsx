87f67540dc91d4f15b597e4b3e3b1a25b8890867
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 15:38:07 +0530

    Update DashFooter.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/DashFooter.jsx
+++ src/components/Dashboard/DashFooter.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const DashFooter = () => {
   return (
     <div className='text-gray-400 py-5 px-4 text-sm'>
+        <hr className='border border-gray-400 mb-4'/>
         Copyright &copy; 2024 | JehanKandy | All Right Reserved <br />
         Designed and Developed By : <a href=""><span className="text-blue-500">Jehankandy</span></a>
     </div>
