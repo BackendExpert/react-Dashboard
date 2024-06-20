219fcf189a53182ad9305a35d1a35a3084e657e2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 14:43:24 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Employee from "./components/Employee/Employee";
+import DashHome from "./components/Home/DashHome";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Dashboard/" element={<Dashboard /> }> 
+        <Route path="/Dashboard" element={<Dashboard /> }> 
+           <Route path='Home' element={<DashHome /> } /> 
            <Route path='Employee' element={<Employee /> } /> 
         </Route>
       </Routes>
