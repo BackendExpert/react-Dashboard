a38cc8818c8cd630efeb419426eb7b27dde438c5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 14:25:45 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Employee from "./components/Employee/Employee";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} >
-        
+        <Route path="/Dashboard" element={<Dashboard />} >
+          <Route path="Employee" element={<Employee />} />
         
         </Route>
       </Routes>
