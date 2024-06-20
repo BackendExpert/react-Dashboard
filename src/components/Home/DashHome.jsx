e056fd877f01b23c659b47458ac69935d51eebc8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 16:26:54 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Home/DashHome.jsx
+++ src/components/Home/DashHome.jsx
@@ -4,29 +4,28 @@ import { BsCarFront } from 'react-icons/bs'
 const DashHome = () => {
     
 const dashMenu = [
-    {name: "cars", icons: <BsCarFront />, value: 50, style: "green-500"},
-    {name: "cars", icons: <BsCarFront />, value: 50, style: "red-500"},
-    {name: "cars", icons: <BsCarFront />, value: 50, style: "purple-500"},
-    {name: "cars", icons: <BsCarFront />, value: 50, style: "yellow-500"},
-    {name: "cars", icons: <BsCarFront />, value: 50, style: "blue-500"},    
+    {name: "cars", icons: <BsCarFront />, value: 50, borderStyle: 'border-green-500', style: "green-500"},
+    {name: "cars", icons: <BsCarFront />, value: 50, borderStyle: 'border-red-500', style: "red-500"},
+    {name: "cars", icons: <BsCarFront />, value: 50, borderStyle: 'border-purple-500', style: "purple-500"},
+    {name: "cars", icons: <BsCarFront />, value: 50, borderStyle: 'border-yellow-500', style: "yellow-500"},
+    {name: "cars", icons: <BsCarFront />, value: 50, borderStyle: 'border-blue-500', style: "blue-500"},    
 ]
   return (
     <div>
         <div className="">
+            <h1 className="text-xl font-semibold pb-4 text-gray-300">Dashboard</h1>
             <div className="md:grid grid-cols-4 gap-4">
                 {
                     dashMenu.map((dash) => {
                         return (
-                            <div className="bg-neutral-900 text-white px-4 py-8 rounded">
+                            <div className={`bg-neutral-900 text-white px-4 py-8 rounded border-l-2 shadow-md shadow-red ${dash.borderStyle}`}>
                                 <div className="flex justify-between">
                                     <div className="">
                                         <h1 className="text-3xl font-semibold">{dash.value}</h1>
-                                        <p className="">{dash.name}</p> 
+                                        <p className={`text-${dash.style}`}>{dash.name}</p> 
                                     </div>
                                     <div className="">
-                                        <div className={`bg-${dash.style} rounded-xl p-2`}>
-                                            <p className="text-3xl">{dash.icons}</p>
-                                        </div>
+                                        <p className={`text-4xl text-${dash.style} pt-2`}>{dash.icons}</p>
                                     </div>
                                 </div>
                             </div>
