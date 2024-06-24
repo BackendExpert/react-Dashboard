9c21b7e15747a91e0dda99cb24d2db9ba9ec34b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 10:26:37 +0530

    Update DashNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Dashboard/DashNav.jsx
+++ src/components/Dashboard/DashNav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { BsPersonCircle } from 'react-icons/bs'
 
 const DashNav = () => {
   return (
     <div className='bg-gray-800 text-white py-5 px-4'>
-        <div className="flex">
+        <div className="flex justify-between">
             <div className="">
               ABC Campus
             </div>
-            <div className="">
-
+            <div className="md:block hidden">
+              <BsPersonCircle className='text-2xl'/>
             </div>
         </div>
     </div>
