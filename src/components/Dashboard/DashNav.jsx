141105c60a3aa9d844659f5813d2e4976fb53ff8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 09:57:53 +0530

    Update DashNav.jsx

--- src/components/Dashboard/DashNav.jsx
+++ src/components/Dashboard/DashNav.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const DashNav = () => {
   return (
     <div className='bg-gray-800 text-white py-5 px-4'>
-        ABC Campus
+        <div className="flex">
+            <div className="">
+              ABC Campus
+            </div>
+            <div className="">
+              jehankandy
+            </div>
+        </div>
     </div>
   )
 }
